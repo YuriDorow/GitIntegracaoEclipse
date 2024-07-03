16593eb7d4da8dcb2bb085b0f81d734878f31519
YuriDorow <84820357+YuriDorow@users.noreply.github.com>
2024-07-03 15:09:13 -0300

    Update Main.java
    Alteração de teste via GitHub

--- IntegracaoGitEclipse/src/Main/Main.java
+++ IntegracaoGitEclipse/src/Main/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 	
 		System.out.println("Olá mundo!");
-		System.out.println("Tô odiando isso aqui");
+		System.out.println("Alterando do Git para o Eclipse");
 	}
 
 }
